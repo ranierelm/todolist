164f1a494321a8fe4c848ba2333cdbad70baf033
Raniere Lima <raniereprojeto@gmail.com>
2022-03-20 17:06:08 -0300

    remove and save tasks

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'models/item.dart';
 
 void main() => runApp(MyApp());
@@ -22,10 +25,6 @@ class HomePage extends StatefulWidget {
 
   HomePage() {
     items = [];
-
-    items.add(Item(title: "Item 1", done: false));
-    items.add(Item(title: "Item 2", done: false));
-    items.add(Item(title: "Item 3", done: false));
   }
 
   @override
@@ -47,7 +46,38 @@ class _HomePageState extends State<HomePage> {
       );
 
       newTaskController.clear();
+      save();
     });
+  }
+
+  void remove(int index) {
+    setState(() {
+      widget.items.removeAt(index);
+      Fluttertoast.showToast(msg: "ITEM REMOVIDO");
+      save();
+    });
+  }
+
+  Future load() async {
+    var prefs = await SharedPreferences.getInstance();
+    var data = prefs.getString('data');
+
+    if (data != null) {
+      Iterable decoded = jsonDecode(data);
+      List<Item> result = decoded.map((x) => Item.fromJson(x)).toList();
+      setState(() {
+        widget.items = result;
+      });
+    }
+  }
+
+  save() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString('data', jsonEncode(widget.items));
+  }
+
+  _HomePageState() {
+    load();
   }
 
   @override
@@ -74,15 +104,24 @@ class _HomePageState extends State<HomePage> {
         itemBuilder: (BuildContext cntx, int index) {
           final item = widget.items[index];
 
-          return CheckboxListTile(
-              title: Text(item.title!),
-              key: Key(item.title!),
-              value: item.done,
-              onChanged: (value) {
-                setState(() {
-                  item.done = value;
-                });
-              });
+          return Dismissible(
+            child: CheckboxListTile(
+                title: Text(item.title!),
+                value: item.done,
+                onChanged: (value) {
+                  setState(() {
+                    item.done = value;
+                    save();
+                  });
+                }),
+            key: Key(item.title!),
+            background: Container(
+              color: Colors.blue.withOpacity(0.2),
+            ),
+            onDismissed: (direction) {
+              remove(index);
+            },
+          );
         },
       ),
       floatingActionButton: FloatingActionButton(
